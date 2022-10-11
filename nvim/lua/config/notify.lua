d901cdd88dfe4fcbc0f3caac55754efe0fba8b88
Lior Levin <tdm.levin.lior@gmail.com>
2022-10-11 13:56:39 +0300

    Commented out lsp hookage

--- nvim/lua/config/notify.lua
+++ nvim/lua/config/notify.lua
@@ -14,22 +14,22 @@ local notify = require('notify')
 vim.notify = notify
 
 -- Hook on lsp's showMessage handler
-vim.lsp.handlers['window/showMessage'] = function(_, result, ctx)
-  local client = vim.lsp.get_client_by_id(ctx.client_id)
-  local lvl = ({
-    'ERROR',
-    'WARN',
-    'INFO',
-    'DEBUG',
-  })[result.type]
-  notify({ result.message }, lvl, {
-    title = 'LSP | ' .. client.name,
-    timeout = 10000,
-    keep = function()
-      return lvl == 'ERROR' or lvl == 'WARN'
-    end,
-  })
-end
+-- vim.lsp.handlers['window/showMessage'] = function(_, result, ctx)
+--   local client = vim.lsp.get_client_by_id(ctx.client_id)
+--   local lvl = ({
+--     'ERROR',
+--     'WARN',
+--     'INFO',
+--     'DEBUG',
+--   })[result.type]
+--   notify({ result.message }, lvl, {
+--     title = 'LSP | ' .. client.name,
+--     timeout = 10000,
+--     keep = function()
+--       return lvl == 'ERROR' or lvl == 'WARN'
+--     end,
+--   })
+-- end
 
 -- When support for ui_attach is added, this will:
 -- Hook on vim's messages
